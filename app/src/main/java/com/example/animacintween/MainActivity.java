a42aff5b1b6d4f399274ee59fa1629a954ab5dc3
juacarr <juacarrd@v-espino.es>
2021-11-03 13:54:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/animacintween/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.animacintween;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView texto = (TextView) findViewById(R.id.textView);
+        Animation animacion = AnimationUtils.loadAnimation(this,
+                R.anim.animacion);
+        texto.startAnimation(animacion);
+    }
+}
